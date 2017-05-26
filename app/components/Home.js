9bba9322599d9e615abf3bcd9bfe4709ad48f71e
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2017-05-26 16:03:17 +0300

    Removed moi

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className='home-container'>
-        <h1>moi</h1>
+        <h1>Battle your friends</h1>
         <Link className='button' to ='/battle'>
           Battle
         </Link>
